bbbc413e55a8b2e4890abfdd34e2146506660edb
Pelmeshka127 <111414881+Pelmeshka127@users.noreply.github.com>
2022-09-08 21:55:48 +0300

    onegin.h

--- onegin.h
+++ onegin.h
@@ -1,6 +1,12 @@
 #ifndef ONEGIN_H_
 #define ONEGIN_H_
 
-void Copy_To_File (FILE * old, FILE * nw);
+void Copy_File (FILE * old, FILE * nw);
+
+int Num_Of_Strings (FILE * old);
+
+int Max_String_Len (FILE * old);
+
+void Sorted_Strings (char *strings[], int ch, FILE * nw);
 
 #endif
