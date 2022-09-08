80b13b4424c8e52f05fcbfa3301355d4ed19f19d
Pelmeshka127 <111414881+Pelmeshka127@users.noreply.github.com>
2022-09-08 21:55:09 +0300

    onegin.cpp

--- onegin.cpp
+++ onegin.cpp
@@ -1,28 +1,81 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 #include "onegin.h"
 
-void Copy_To_File (FILE * old, FILE * nw)
+void Copy_File (FILE * old, FILE * nw)
 {
     old = fopen("old.txt", "r");
-    nw = fopen("nw.txt", "w");
+    nw  = fopen("nw.txt",  "w");
     int ch = 0;
     
     while ((ch = getc(old)) != EOF)
     {
-        if (isalpha(ch) || ch == '\n')
+        if (isalpha(ch) || ch == '\n' || ch == ' ')
         {
             putc(ch, nw);
         }
-        else if (ch == ' ')
+    }
+    fclose(old);
+    fclose(nw);
+}
+
+int Num_Of_Strings (FILE * nw)
+{
+    nw = fopen("nw.txt", "r");
+    int ch = 0, count = 0;
+
+    while ((ch = getc(nw)) != EOF)
+    {
+        if (ch == '\n')
         {
-            ch = '\0';
-            putc('\n', nw);
-        }
-        else
-        {
-            ch = '\0';
+            count++;
         }
     }
+    return count;
+    fclose(nw);
+}
+
+int Max_String_Len (FILE * old)
+{
+    int len = 0, ch = 0, max_len = 0;
+    old = fopen("old.txt", "r");
+
+    while ((ch = getc(old)) != EOF)
+    {
+        len += (ch != '\n');
+        if (ch == '\n')
+        {
+            if (len > max_len)
+            {
+                max_len = len;
+            }
+            len = 0;
+        }
+    }
+    return max_len;
+    fclose(old);
+}
+
+
+void Sorted_Strings (char *strings[], int ch, FILE * nw)
+{
+    nw = fopen("nw.txt", "r+");
+    int i = 0, j = 0;
+    char * temp = 0;
+    
+    for (i = 0; i < ch - 1; i++)
+    {
+        for (j = i + 1; j < ch; j++)
+        {
+            if (strcmp(strings[i], strings[j]) > 0)
+            {
+                temp = strings[i];
+                strings[i] = strings[j];
+                strings[j] = temp;
+            }
+        }
+    }
+    fclose(nw);
 }
