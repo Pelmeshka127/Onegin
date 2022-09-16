0d6dbd3a4d3013c2e645907ddce9745796132b59
Pelmeshka127 <111414881+Pelmeshka127@users.noreply.github.com>
2022-09-17 01:10:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,22 +4,58 @@
 #include <ctype.h>
 #include <assert.h>
 
-#include "onegin.h"
+#include "text_functions.h"
 
 int main(void)
 {
-    FILE * old = NULL, * nw = NULL;
-    Copy_File(old, nw);
+    struct Text_Info Onegin = {
+        .f_onegin = NULL, .file_name = NULL,
+        .point_array = NULL, .strings = NULL,
+        .nstr = 0,
+    };
+
+    Constructor(&Onegin);
+
+    Read_File (&Onegin);
+
+    Destructor(&Onegin);
+
+
+    /*
+    //
+    struct Text_Info Onegin = {};
+
+    // ctor 
+
+    // read_strings
+    // 
+
+    // sort_onegin()
+    // 
+
+    // print
+
+    // dtor 
 
     int const Num_str = Num_Of_Strings(old);
     int const Max_len = Max_String_Len(old);
+
     printf("Count of strings is %d.\n"
            "Max count of len is %d.\n"
            , Num_str, Max_len);
-    int ct = 0, k = 0;
-    char string[Num_str][Max_len];
+
+    int ct = 0, k = 0;             //
+    char string[Num_str][Max_len]; //
     char *point_string[Num_str];
-    nw = fopen("nw.txt", "r");
+    
+    nw = fopen("nw.txt", "r"); // open_file
+    // errno, perror, strerr 
+
+    // calloc char*
+    // fgetc -> size -> calloc
+    // fseek, rewind
+    // fgets
+    // fread, calloc
 
     while (ct < Num_str && fgets(string[ct], Max_len, nw) != NULL
            && string[ct][0] != '\0')
@@ -28,13 +64,15 @@ int main(void)
             ct++;
         }
     
-    Sorted_Strings (point_string, ct, nw);
+    Sorted_Strings(point_string, ct, nw);  
 
     fclose(nw);
 
-    for (k = 0; k < ct; k++)
+    for (k = 0; k < ct; k++) //name, definition
     {
         printf("%s\n", point_string[k]);
     }
+    */
+
     return 0;
 }
