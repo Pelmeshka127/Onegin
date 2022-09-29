225172de386ed257cfa9aefff7092834bc340081
Pelmeshka127 <111414881+Pelmeshka127@users.noreply.github.com>
2022-09-29 22:47:37 +0300

    text_functions.h

--- text_functions.h
+++ text_functions.h
@@ -1,26 +1,39 @@
 #ifndef TEXT_FUNCTIONS_H_
 #define TEXT_FUNCTIONS_H_
 
-int Str_Comporator(const char * s1, const char * s2);
-
 struct Text_Info {
-    FILE * f_onegin;
-    char * file_name;
-    char ** strings; 
-    char * point_array;
-    int nstr;
+    FILE * input_fonegin, * out_fonegin;
+    char * text_string;
+    unsigned long nstr, nch;
 };
 
-void Constructor(struct Text_Info * Onegin);
+struct Arr_Struct_Info {
+    char * string;
+    int len_str;
+};
 
-void Num_Str(struct Text_Info * Onegin);
+/// function compares two strings
+int Str_Comporator(const char * s1, const char * s2);
 
-int Len_Str(struct Text_Info * Onegin, int num_str);
+/// function opens files
+int Constructor(struct Text_Info * Onegin, char * input_file, char * out_file);
 
-void Read_File(struct Text_Info * Onegin);
+/// function finds out count of stings in f_open_onegin
+int Num_Ch_Str(struct Text_Info * Onegin);
 
-void My_Sort(struct Text_Info * Onegin);
+/// function reads input file
+unsigned long Read_File(struct Text_Info * Onegin);
 
-void Destructor(struct Text_Info * Onegin);
+/// function make strings from input file
+int Make_Strings(struct Text_Info * Onegin, struct Arr_Struct_Info * Strings);
+
+/// function sortes strings from input file
+int My_Sort(struct Text_Info * Onegin, struct Arr_Struct_Info * Strings);
+
+/// function writes strings to the out file 
+int Make_Out_File(struct Text_Info * Onegin, struct Arr_Struct_Info * Strings);
+
+/// function closes files and clear dedicated memory
+void Destructor(struct Text_Info * Onegin, struct Arr_Struct_Info * Strings);
 
 #endif
