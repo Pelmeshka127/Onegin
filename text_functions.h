cd89c958be01536aae7f01d9453a4fa34cbf8a94
Pelmeshka127 <111414881+Pelmeshka127@users.noreply.github.com>
2022-10-02 12:52:57 +0300

    text_functions.h

--- text_functions.h
+++ text_functions.h
@@ -2,9 +2,9 @@
 #define TEXT_FUNCTIONS_H_
 
 struct Text_Info {
-    FILE * input_fonegin, * out_fonegin;
+    FILE * input_fonegin, * sort_fonegin, * backsort_fonegin;
     char * text_string;
-    unsigned long nstr, nch;
+    unsigned long n_str, n_symb;
 };
 
 struct Arr_Struct_Info {
@@ -15,14 +15,15 @@ struct Arr_Struct_Info {
 /// function compares two strings
 int Str_Comporator(const char * s1, const char * s2);
 
-/// function opens files
-int Constructor(struct Text_Info * Onegin, char * input_file, char * out_file);
+///
+int Back_Cmp(const void * p1, const void * p2);
 
-/// function finds out count of stings in f_open_onegin
-int Num_Ch_Str(struct Text_Info * Onegin);
+/// function opens files
+int Constructor(struct Text_Info * Onegin, char * input_file,
+                char * sortout_file, char * backsortout_file);
 
 /// function reads input file
-unsigned long Read_File(struct Text_Info * Onegin);
+int Read_File(struct Text_Info * Onegin);
 
 /// function make strings from input file
 int Make_Strings(struct Text_Info * Onegin, struct Arr_Struct_Info * Strings);
@@ -30,8 +31,14 @@ int Make_Strings(struct Text_Info * Onegin, struct Arr_Struct_Info * Strings);
 /// function sortes strings from input file
 int My_Sort(struct Text_Info * Onegin, struct Arr_Struct_Info * Strings);
 
+/// functions sortes strings in reverse order
+int My_Backsort(struct Text_Info * Onegin, struct Arr_Struct_Info * Strings);
+
+/// function swaps strings
+int Onegin_Swap(struct Arr_Struct_Info * Strings, int i_elem, int j_elem);
+
 /// function writes strings to the out file 
-int Make_Out_File(struct Text_Info * Onegin, struct Arr_Struct_Info * Strings);
+int Make_Outfile(struct Text_Info * Onegin, struct Arr_Struct_Info * Strings, FILE * fp);
 
 /// function closes files and clear dedicated memory
 void Destructor(struct Text_Info * Onegin, struct Arr_Struct_Info * Strings);
