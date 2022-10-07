c23196e72836a578a96498b48b1f609aa28bf6dd
Pelmeshka127 <111414881+Pelmeshka127@users.noreply.github.com>
2022-10-07 13:00:16 +0300

    main.cpp
    I added:
    Input files using command line arguments
    Openning(closing) files in main.cpp, not in Ctor(Dtor)
    Returning result file

--- main.cpp
+++ main.cpp
@@ -3,49 +3,56 @@
 #include <string.h>
 #include <ctype.h>
 #include <assert.h>
+#include <errno.h>
 
 #include "text_functions.h"
 
-int main(void)
+int main(int argc, char ** argv)
 {
-    Text_Info Onegin = {
-        .input_fonegin = NULL,
-        .sort_fonegin = NULL,
-        .backsort_fonegin = NULL,
-        .text_string = NULL,
-        .n_str = 0,
-        .n_symb = 0,
-    };
+    Text_Info Onegin = {};
+
+    FILE * input_file = NULL, * sortout_file = NULL, * backsortout_file = NULL, * res_file = NULL;
+
+    input_file = fopen(argv[1], "rb");
+    sortout_file = fopen(argv[2], "wb");
+    backsortout_file = fopen(argv[3], "wb");
+    res_file = fopen(argv[4], "wb");
     
-    char input_file[30];
-    char sortout_file[30];
-    char backsortout_file[30];
-    puts("Enter name of input file");
-    scanf("%s", input_file);
-
-    puts("Enter name of sorted out file");
-    scanf("%s", sortout_file);
-
-    puts("Enter name of back sorted out file");
-    scanf("%s", backsortout_file);
+    Onegin_Ctor(&Onegin, input_file, sortout_file, backsortout_file, res_file, 
+                argv[1], argv[2], argv[3], argv[4]);   
     
-    Constructor(&Onegin, input_file, sortout_file, backsortout_file);   
+    if (Onegin_Read(&Onegin, input_file) == -1)
+    {
+        fprintf(stderr, "Error while calling Onegin_Read\n");
+    }
+
+    /// adding orig text to res file
+    Onegin_MakeOutfile(&Onegin, res_file);
+
+    Onegin_Sort(&Onegin, Right_LexCmp);
+
+    /// adding fisrt sorted text to the sortout_file
+    Onegin_MakeOutfile(&Onegin, sortout_file);
+    /// adding fisrt sorted text to the res file
+    Onegin_MakeOutfile(&Onegin, res_file); 
     
-    Read_File(&Onegin);
+    Onegin_Sort(&Onegin, Back_LexCmp); 
 
-    Arr_Struct_Info * Strings = (Arr_Struct_Info *) calloc (Onegin.n_str+1, sizeof(Arr_Struct_Info));
+    /// adding second sorted text to the backsortout_file
+    Onegin_MakeOutfile(&Onegin, backsortout_file);
+    /// adding second sorted text to the res file
+    Onegin_MakeOutfile(&Onegin, res_file);
 
-    Make_Strings(&Onegin, Strings);
+    Onegin_Dtor(&Onegin);
 
-    My_Sort(&Onegin, Strings);
+    if (fclose(input_file) != 0)
+        fprintf(stderr, "Error, file %s wasn't closed %s\n", argv[1], strerror(errno));
 
-    Make_Outfile(&Onegin, Strings, Onegin.sort_fonegin);
-    
-    My_Backsort(&Onegin, Strings);
+    if (fclose(sortout_file) != 0)
+        fprintf(stderr, "Error, file %s wasn't closed %s\n", argv[2], strerror(errno));
 
-    Make_Outfile(&Onegin, Strings, Onegin.backsort_fonegin);
-
-    Destructor(&Onegin, Strings);
+    if (fclose(backsortout_file) != 0)
+        fprintf(stderr, "Error, file %s wasn't closed %s\n", argv[3], strerror(errno));
     
     puts("Prog finished successfully");
 
