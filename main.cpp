1cfe8738ceb4540c1aa4555e4067f6f46779e500
Pelmeshka127 <111414881+Pelmeshka127@users.noreply.github.com>
2022-10-20 20:40:01 +0300

    Update main.cpp
    adding conditional compilation and calling to sort

--- main.cpp
+++ main.cpp
@@ -10,38 +10,64 @@
 int main(int argc, char ** argv)
 {
     Text_Info Onegin = {};
-
-    FILE * input_file = NULL, * sortout_file = NULL, * backsortout_file = NULL, * res_file = NULL;
-
-    input_file = fopen(argv[1], "rb");
-    sortout_file = fopen(argv[2], "wb");
-    backsortout_file = fopen(argv[3], "wb");
-    res_file = fopen(argv[4], "wb");
     
-    Onegin_Ctor(&Onegin, input_file, sortout_file, backsortout_file, res_file, 
-                argv[1], argv[2], argv[3], argv[4]);   
-    
-    if (Onegin_Read(&Onegin, input_file) == -1)
+    FILE *input_file = NULL, *sortout_file = NULL, *backsortout_file = NULL, *res_file = NULL;
+
+    if (Check_Cmdline_Arg(argc) == Cmdline_Error)
+        return Cmdline_Error;
+
+    if ((input_file = fopen(argv[1], "rb")) == NULL)
     {
-        fprintf(stderr, "Error while calling Onegin_Read\n");
+        ONEGIN_ERROR();
+        return File_Is_Missed;
     }
 
-    /// adding orig text to res file
-    Onegin_MakeOutfile(&Onegin, res_file);
+    if ((sortout_file = fopen(argv[2], "wb")) == NULL)
+    {
+        ONEGIN_ERROR();
+        return File_Is_Missed;
+    }
 
-    Onegin_Sort(&Onegin, Right_LexCmp);
-
-    /// adding fisrt sorted text to the sortout_file
-    Onegin_MakeOutfile(&Onegin, sortout_file);
-    /// adding fisrt sorted text to the res file
-    Onegin_MakeOutfile(&Onegin, res_file); 
+    if ((backsortout_file = fopen(argv[3], "wb")) == NULL)
+    {
+        ONEGIN_ERROR();
+        return File_Is_Missed;
+    }
     
-    Onegin_Sort(&Onegin, Back_LexCmp); 
+    if ((res_file = fopen(argv[4], "wb")) == NULL)
+    {
+        ONEGIN_ERROR();
+        return File_Is_Missed;
+    }
 
-    /// adding second sorted text to the backsortout_file
-    Onegin_MakeOutfile(&Onegin, backsortout_file);
-    /// adding second sorted text to the res file
-    Onegin_MakeOutfile(&Onegin, res_file);
+    if (Onegin_Read(&Onegin, input_file) == Reading_File_Error)
+        return Reading_File_Error;
+
+    Onegin_Print_To_File(&Onegin, res_file); ///adding orig text to res file
+
+#if SORT_TYPE == C_QSORT
+    Onegin_Qsort(&Onegin, 0, Onegin.n_str - 1, Direct_Lex_Cmp);
+#elif SORT_TYPE == MY_QSORT
+    qsort(Onegin.Strings, Onegin.n_str, sizeof(Onegin_Line_Info), Direct_Lex_Cmp);
+#else
+    return -1;
+#endif
+
+    Onegin_Print_To_File(&Onegin, sortout_file); /// adding fisrt sorted text to the sortout_file
+
+    Onegin_Print_To_File(&Onegin, res_file); /// adding fisrt sorted text to the res file
+
+#if SORT_TYPE == C_SORT
+    Onegin_Qsort(&Onegin, 0, Onegin.n_str - 1, Reverse_Lex_Cmp);
+#elif SORT_TYPE == MY_QSORT
+    qsort(Onegin.Strings, Onegin.n_str, sizeof(Onegin_Line_Info), Reverse_Lex_Cmp);
+#else
+    return - 1;
+#endif
+
+    Onegin_Print_To_File(&Onegin, backsortout_file); /// adding second sorted text to the backsortout_file
+
+    Onegin_Print_To_File(&Onegin, res_file); /// adding second sorted text to the res file
 
     Onegin_Dtor(&Onegin);
 
@@ -54,7 +80,8 @@ int main(int argc, char ** argv)
     if (fclose(backsortout_file) != 0)
         fprintf(stderr, "Error, file %s wasn't closed %s\n", argv[3], strerror(errno));
     
-    puts("Prog finished successfully");
+    if (fclose(res_file) != 0)
+        fprintf(stderr, "Error, file %s wasn't closed %s\n", argv[4], strerror(errno));
 
     return 0;
 }
